da9595ff480f4b13814838c1b0517782cfdea1df
ploop <106459637+meifi-moogle@users.noreply.github.com>
2023-02-06 05:08:15 +0100

    Remove console.logs in the playRound function, make a new function to play 3 rounds, display the winner and final score

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function getComputerChoice() {
-  
+
     let num = Math.floor(Math.random() * 3) + 1;
     let result = '';
-    if (num === 1) {result = 'rock'}
-    if (num === 2) {result = 'paper'}
-    if (num === 3) {result = 'scissors'}
+    if (num === 1) { result = 'rock' }
+    if (num === 2) { result = 'paper' }
+    if (num === 3) { result = 'scissors' }
     return result;
 }
 
@@ -18,12 +18,31 @@ function playRound(playerSelection, computerSelection) {
         scissors: 'paper'
     }
 
-    if (computerSelection === rules[playerSelection]) 
-    {console.log('Player wins')}
-    if (playerSelection === rules[computerSelection]) 
-    {console.log('Computer wins')}
-    if (playerSelection === computerSelection)
-    {console.log('Draw!')}
+    if (computerSelection === rules[playerSelection]) { return 'Player wins' }
+    if (playerSelection === rules[computerSelection]) { return 'Computer wins' }
+    if (playerSelection === computerSelection) { return 'Draw!' }
 }
 
-playRound();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 3; i++) {
+
+        let result = playRound()
+        if (result === 'Player wins') { playerScore += 1 };
+        if (result === 'Computer wins') { computerScore += 1 };
+        console.log(`${playerScore} to ${computerScore}`)
+    }
+    if (playerScore > computerScore) 
+    {console.log(`Player won the game ${playerScore} to ${computerScore}`)}
+
+    if (playerScore < computerScore)
+     {console.log(`Computer won the game ${computerScore} to ${playerScore}`)}
+
+    if (playerScore === computerScore) 
+    {console.log(`Wow, draw with ${playerScore} to ${computerScore}`)}
+}
+game();
+
+
